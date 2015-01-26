a455ec7e3e2e4c35fd71597bbae1e0dfbd9a962a
t-lucas <lucas.turuani@gmail.com>
2015-01-26 16:15:15 +0100

    Ajout des controller pour chaque page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,62 @@
+//Création des pages
  var app = angular.module('playground', ['ngRoute']);
         app.config(function($routeProvider){
             $routeProvider
-                    .when('/', {templateUrl: 'template/home.html'})
-                    .when('/carte', {templateUrl: 'template/carte.html'})
-                    .when('/choix-personnage', {templateUrl: 'template/choix-personnage.html'})
-                    .when('/temps-travail', {templateUrl: 'template/temps-travail.html'})
-                    .when('/comparatif', {templateUrl: 'template/comparatif.html'})
-                    .when('/about', {templateUrl: 'template/about.html'})
+                    .when('/', {
+	                    templateUrl: 'template/home.html',
+	                    controller: 'homeCtrl'
+	                    })
+                    .when('/carte', {
+	                    templateUrl: 'template/carte.html',
+	                    controller: 'carteCtrl'
+	                    })
+                    .when('/choix-personnage', {
+	                    templateUrl: 'template/choix-personnage.html',
+	                    controller: 'personnageCtrl'
+	                    })
+                    .when('/temps-travail', {
+	                    templateUrl: 'template/temps-travail.html',
+	                    controller: 'tempstravailCtrl'
+	                    })
+                    .when('/comparatif', {
+	                    templateUrl: 'template/comparatif.html',
+	                    controller: 'comparatifCtrl'
+	                    })
+                    .when('/about', {
+	                    templateUrl: 'template/about.html',
+	                    controller: 'aboutCtrl'
+	                    })
                     .otherwise({redirectTo: '/'})
         });
-        app.controller('LayoutCtrl', function($scope, $log) {
+        
+//création des controller
+
+	//controller home
+        app.controller('homeCtrl', function($scope, $log) {
+        $log.debug('angular is ready');
+      });
+    
+    //controller carte
+        app.controller('carteCtrl', function($scope, $log) {
+        $log.debug('angular is ready');
+      });
+      
+    //controller choix personnage
+        app.controller('personnageCtrl', function($scope, $log) {
+        $log.debug('angular is ready');
+      });
+      
+    //controller temps travail
+        app.controller('tempstravailCtrl', function($scope, $log) {
+        $log.debug('angular is ready');
+      });
+    
+    //controller comparatif
+        app.controller('comparatifCtrl', function($scope, $log) {
+        $log.debug('angular is ready');
+      });
+    
+    //controller about
+        app.controller('aboutCtrl', function($scope, $log) {
         $log.debug('angular is ready');
       });
