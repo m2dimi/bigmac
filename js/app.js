4f5f090ed53c83a3f56087be48c1e1d001276205
t-lucas <lucas.turuani@gmail.com>
2015-01-29 15:51:18 +0100

    Display character Thumbnails
    Display all character thumb for the character choice page. A  service
    (factory) get the data and the controller display the pictures on the
    page with a ng-repeat

--- js/app.js
+++ js/app.js
@@ -32,22 +32,174 @@
 	                    })
                     .otherwise({redirectTo: '/'})
         });
-        
+//factory
+app.factory('dataFactory', function($http){
+	/*
+var obj = {content:null};
+
+    $http.get('data/data.json').success(function(data) {
+        // you can do some processing here
+        obj.content = data;
+    });    
+
+    return obj; 
+*/
+var factory = [
+    {
+		"id" : "1",
+        "country" : "France",
+        "worktime": "13",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "2",
+        "country" : "Brésil",
+        "worktime": "120",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "3",
+        "country" : "Mexique",
+        "worktime": "31",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "4",
+        "country" : "Australie",
+        "worktime": "09",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "5",
+        "country" : "Azerbajan",
+        "worktime": "368",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "6",
+        "country" : "Amerique",
+        "worktime": "10",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "7",
+        "country" : "Russie",
+        "worktime": "30",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "8",
+        "country" : "Argentine",
+        "worktime": "71",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "9",
+        "country" : "Egypte",
+        "worktime": "21.16",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "10",
+        "country" : "Emirates",
+        "worktime": "13",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "11",
+        "country" : "Japon",
+        "worktime": "08",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "12",
+        "country" : "Chine",
+        "worktime": "35",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "13",
+        "country" : "Afrique du sud",
+        "worktime": "49",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "14",
+        "country" : "Inde",
+        "worktime": "339",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "15",
+        "country" : "Uk",
+        "worktime": "15",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "16",
+        "country" : "Roumanie",
+        "worktime": "52",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "17",
+        "country" : "Grèce",
+        "worktime": "32",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    },
+    {
+		"id" : "18",
+        "country" : "Maroc",
+        "worktime": "345",
+        "thumb" : "ressources/caractere/test.jpg",
+		"url_img" : "ressources/caractere/test.jpg"
+    }
+
+];
+
+		
+	return factory;
+});
 //création des controller
 
 	//controller home
-        app.controller('homeCtrl', function($scope, $log, $routeParams) {
+        app.controller('homeCtrl', function($scope, $log, $routeParams, dataFactory) {
         $log.debug('angular is ready');
             $log.debug('test', $routeParams);
             $scope.countries = [1,2,3];
             $routeParams.countryA = 'france';
             $routeParams.countryB = 'belgique';
-            
+            /*
+$http.get('data/data.json').success(function(data){
+		$log.debug('fichier chargé');
+	}).
+	error(function(data){
+		$log.debug('error');
+	});
+*/
 
+			 $log.debug('data', dataFactory.content);
 
-
-            $scope.choose = function(country){
-                $scope.countries=[1,3,54]
+			$scope.countries = dataFactory.obj;
+			$scope.choose = function(country){
+                 
             }
       });
     
@@ -57,8 +209,10 @@
       });
       
     //controller choix personnage
-        app.controller('personnageCtrl', function($scope, $log) {
+        app.controller('personnageCtrl', function($scope, $log, dataFactory) {
         $log.debug('angular is ready');
+        $scope.countries = dataFactory;
+        
       });
       
     //controller temps travail
