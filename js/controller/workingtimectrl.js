3d91a4c24cf261e95a6b14b5b99851af0f27af27
D-Mickael <duvernay.mickael@laposte.net>
2015-02-12 21:49:08 +0100

    maj worktime

--- js/controller/workingtimectrl.js
+++ js/controller/workingtimectrl.js
@@ -14,11 +14,16 @@
             $scope.ecran = 'home';
 
             dataFactory.GetDataCountry(function(data){
-                var countrySelect = $routeParams.countryA;
-                var idcountry = $filter('filter')(data, countrySelect);
-                $scope.worktimeselection = $filter('filter')(data, countrySelect, true)[0].worktime;
-                $scope.countryselection = $filter('filter')(data, countrySelect, true)[0].perso;
-                $scope.prenomselection = $filter('filter')(data, countrySelect, true)[0].prenom;
+                var countrySelect1 = $routeParams.countryA;
+                var countrySelect2 = $routeParams.countryB;
+                var idcountry = $filter('filter')(data, countrySelect1);
+                $scope.worktimeselection1 = $filter('filter')(data, countrySelect1, true)[0].worktime;
+                $scope.countryselection1 = $filter('filter')(data, countrySelect1, true)[0].perso;
+                $scope.prenomselection1 = $filter('filter')(data, countrySelect1, true)[0].prenom;
+
+                $scope.worktimeselection2 = $filter('filter')(data, countrySelect2, true)[0].worktime;
+                $scope.countryselection2 = $filter('filter')(data, countrySelect2, true)[0].perso;
+                $scope.prenomselection2 = $filter('filter')(data, countrySelect2, true)[0].prenom;
             })
 
             
