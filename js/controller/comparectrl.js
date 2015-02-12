0c04645ffba45febbd00651997f269d8a0cc1a61
t-lucas <lucas.turuani@gmail.com>
2015-02-12 18:10:07 +0100

    compare page beginning

--- js/controller/comparectrl.js
+++ js/controller/comparectrl.js
@@ -24,4 +24,45 @@
                 $scope.prenomperso1 = $filter('filter')(data, countrySelect1, true)[0].prenom;
                 $scope.prenomperso2 = $filter('filter')(data, countrySelect2, true)[0].prenom;
             })
+
+            $('#page1compare').hide();
+            $('#page2compare').hide();
+            $('#page3compare').hide();
+            $('#page4compare').hide();
+            $('#page5compare').hide();
+            $('#page6compare').hide();
+            $('#page7compare').hide();
+
+            $scope.goPage1 = function(){
+                $('#homecompare').hide();
+                $('#page1compare').show();
+            }
+            $scope.goPage2 = function(){
+                $('#page1compare').hide();
+                $('#page2compare').show();
+            }
+            $scope.goPage3 = function(){
+                $('#page2compare').hide();
+                $('#page3compare').show();
+            }
+            $scope.goPage4 = function(){
+                $('#page3compare').hide();
+                $('#page4compare').show();
+            }
+            $scope.goPage5 = function(){
+                $('#page4compare').hide();
+                $('#page5compare').show();
+            }
+            $scope.goPage6 = function(){
+                $('#page5compare').hide();
+                $('#page6compare').show();
+            }
+            $scope.goPage7 = function(){
+                $('#page6compare').hide();
+                $('#page7compare').show();
+            }
+            $scope.goPage8 = function(){
+                $('#page7compare').hide();
+                $('#page8compare').show();
+            }
       });
