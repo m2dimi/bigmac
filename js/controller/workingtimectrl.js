6a9696d0a1fb3952567a45f9969dee5ea77ca352
D-Mickael <duvernay.mickael@laposte.net>
2015-02-13 23:11:36 +0100

    maj worktime

--- js/controller/workingtimectrl.js
+++ js/controller/workingtimectrl.js
@@ -1,9 +1,10 @@
 //CONTROLLERS
 //Each page have his controller. We need to write the code in each controllers
+var worktimeselection1;
+var worktimeselection2;
 
  //controller temps travail
         app.controller('tempstravailCtrl', function($scope, $log, $routeParams, dataFactory, $filter) {
-
             //Display the name of the selected countries
             $scope.model = {
                 dataurl1: $routeParams.countryA,
@@ -17,16 +18,36 @@
                 var countrySelect1 = $routeParams.countryA;
                 var countrySelect2 = $routeParams.countryB;
                 var idcountry = $filter('filter')(data, countrySelect1);
-                $scope.worktimeselection1 = $filter('filter')(data, countrySelect1, true)[0].worktime;
+                worktimeselection1 = $filter('filter')(data, countrySelect1, true)[0].worktime;
+                $scope.worktimeselection1 = worktimeselection1;
                 $scope.countryselection1 = $filter('filter')(data, countrySelect1, true)[0].perso;
                 $scope.prenomselection1 = $filter('filter')(data, countrySelect1, true)[0].prenom;
 
-                $scope.worktimeselection2 = $filter('filter')(data, countrySelect2, true)[0].worktime;
+                worktimeselection2 = $filter('filter')(data, countrySelect2, true)[0].worktime;
+                $scope.worktimeselection2 = worktimeselection2;
                 $scope.countryselection2 = $filter('filter')(data, countrySelect2, true)[0].perso;
                 $scope.prenomselection2 = $filter('filter')(data, countrySelect2, true)[0].prenom;
             })
 
-            
+            $scope.bigmac_calcul= function() {
+                var time =  $("#time").val();
+                var min = time * 60;
+                var totalbigmac1;
+                var totalbigmac2;
+
+                $scope.totalbigmac1 = min/worktimeselection1;
+                $scope.totalbigmac2 = min/worktimeselection2;
+            }
+
 
       });
-    
+
+
+
+
+function refresh() {
+    var tmp = new Date();
+    var img = $("#img_machine")[0];
+    img.src = img.src + '?' + tmp.getTime();
+}
+
