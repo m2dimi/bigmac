c3f518e926566d41141e25b7a72dbcb172dfc851
D-Mickael <duvernay.mickael@laposte.net>
2015-02-17 21:55:40 +0100

    ajout timer

--- js/controller/workingtimectrl.js
+++ js/controller/workingtimectrl.js
@@ -2,6 +2,7 @@
 //Each page have his controller. We need to write the code in each controllers
 var worktimeselection1;
 var worktimeselection2;
+var nbr_time ="30";
 
  //controller temps travail
         app.controller('tempstravailCtrl', function($scope, $timeout, $log, $routeParams, dataFactory, $filter) {
@@ -9,7 +10,7 @@ var worktimeselection2;
             $scope.model = {
                 dataurl1: $routeParams.countryA,
                 dataurl2: $routeParams.countryB
-            }
+            };
             $scope.countryA = $routeParams.countryA;
             $scope.countryB = $routeParams.countryB;
             $scope.ecran = 'home';
@@ -22,21 +23,25 @@ var worktimeselection2;
                 $scope.countryselection1 = $filter('filter')(data, countrySelect1, true)[0].perso;
                 $scope.prenomselection1 = $filter('filter')(data, countrySelect1, true)[0].prenom;
                 $scope.flag1 = $filter('filter')(data, countrySelect1, true)[0].flag_machine;
+                $scope.nbr_time = "30";
 
                 worktimeselection2 = $filter('filter')(data, countrySelect2, true)[0].worktime;
                 $scope.countryselection2 = $filter('filter')(data, countrySelect2, true)[0].perso;
                 $scope.prenomselection2 = $filter('filter')(data, countrySelect2, true)[0].prenom;
                 $scope.flag2 = $filter('filter')(data, countrySelect2, true)[0].flag_machine;
-            })
+
+
+            });
 
             $scope.bigmac_calcul= function() {
                 $scope.totalbigmac1 =  "";
                 $scope.totalbigmac2 = "";
                 $("#interupt")[0].src = "ressources/machine/on.png";
                 refresh();
+                setInterval(timer, 1000);
                 setTimeout(result, 30000);
                 setTimeout(reload, 30000);
-            }
+            };
 
             function result()
             {
@@ -48,6 +53,22 @@ var worktimeselection2;
                 $scope.totalbigmac2 = Math.round(min/worktimeselection2)*0.5;
                 $timeout(function () {$scope.$apply();});
                 $("#btn_suivant_worktime").fadeIn();
+            }
+
+            function timer()
+            {
+                if(nbr_time != 0) {
+                    --nbr_time;
+                    $scope.nbr_time = nbr_time;
+                    $timeout(function () {
+                        $scope.$apply();
+                    });
+                }
+                else
+                {
+                    clearInterval();
+                    $scope.nbr_time = "30";
+                }
             }
 
       });
